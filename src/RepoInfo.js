723fed443533de6510c0c220afcbfdb42d06b3c6
alishkeir <alishkeir_@hotmail.com>
2021-07-03 19:40:44 +0300

    modified query with some render additions

--- src/RepoInfo.js
+++ src/RepoInfo.js
@@ -4,6 +4,45 @@ const RepoInfo = ({ repo }) => {
     const subscribed = 'px-1 py-0 ms-1 d-inline-block btn btn-sm btn-success';
     const unsubscribed =
         'px-1 py-0 ms-1 d-inline-block btn btn-sm btn-outline-secondary';
+    let license;
+    switch (repo.licenseInfo?.spdxId) {
+        case undefined:
+            license = (
+                <span
+                    className='px-1 py-0 ms-1 d-inline-block btn btn-sm btn-danger'
+                    style={{
+                        fontSize: '.7rem',
+                    }}
+                >
+                    NO LICENSE
+                </span>
+            );
+            break;
+        case 'NOASSERTION':
+            license = (
+                <span
+                    className='px-1 py-0 ms-1 d-inline-block btn btn-sm btn-warning'
+                    style={{
+                        fontSize: '.7rem',
+                    }}
+                >
+                    {repo.licenseInfo.spdxId}
+                </span>
+            );
+            break;
+        default:
+            license = (
+                <span
+                    className='px-1 py-0 ms-1 d-inline-block btn btn-sm btn-dark'
+                    style={{
+                        fontSize: '.7rem',
+                    }}
+                >
+                    {repo.licenseInfo.spdxId}
+                </span>
+            );
+    }
+
     return (
         <li className='list-group-item' key={repo.id.toString()}>
             <div className='d-flex justify-content-between align-items-center'>
@@ -18,18 +57,21 @@ const RepoInfo = ({ repo }) => {
                     </a>
                     <p className='small'>{repo.description}</p>
                 </div>
-                <span
-                    className={
-                        repo.viewerSubscription === 'SUBSCRIBED'
-                            ? subscribed
-                            : unsubscribed
-                    }
-                    style={{
-                        fontSize: '.7rem',
-                    }}
-                >
-                    {repo.viewerSubscription}
-                </span>
+                <div className='text-nowrap ms-3 '>
+                    {license}
+                    <span
+                        className={
+                            repo.viewerSubscription === 'SUBSCRIBED'
+                                ? subscribed
+                                : unsubscribed
+                        }
+                        style={{
+                            fontSize: '.7rem',
+                        }}
+                    >
+                        {repo.viewerSubscription}
+                    </span>
+                </div>
             </div>
         </li>
     );
--- src/query.js
+++ src/query.js
@@ -1,21 +1,24 @@
 const gitHubQuery = {
     query: `
-           {
-             viewer {
-               name
-             }
-             search(query: "user:alishkeir sort:updated-desc", type: REPOSITORY, first: 20) {
-               nodes {
-                 ... on Repository {
-                   name
-                   description
-                   id
-                   url
-                   viewerSubscription
+             {
+               viewer {
+                 name
+               }
+               search(query: "user:alishkeir sort:updated-desc", type: REPOSITORY, first: 50) {
+                 nodes {
+                   ... on Repository {
+                     name
+                     description
+                     id
+                     url
+                     viewerSubscription
+                     licenseInfo {
+                       spdxId
+                     }
+                   }
                  }
                }
-             }
-           }
+             } 
           `,
 };
 
